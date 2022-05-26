3f2bc65625fef52b8e1595ff63fa98e133a61b46
truongthianhhau <truongthianhhau2422002@gmail.com>
2022-05-26 15:38:52 +0700

    chi tiet nguoi dung

--- index.php
+++ index.php
@@ -1,75 +1,6 @@
-<!doctype html>
-<html lang="vi">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
-    <title>Trang tuyển dụng</title>
-
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
-
-    <!-- Bootstrap core CSS -->
-    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
-
-    <style>
-        .avt{
-            border-radius:50% ;
-        }
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-            padding: 10px;
-        }
-        
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-        
-        .bg-container {
-            margin-top: 40px;
-        }
-    </style>
-
-
-    <!-- Custom styles for this template -->
-    <link href="carousel.css" rel="stylesheet">
-</head>
-
-<body>
-
-    <header>
-        <nav style="float: right;" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark navbar-form navbar-left">
-
-
-            <ul style="float: right;" class="navbar-nav ">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="dangnhap.html">Đăng nhập</a>
-                </li>
-            </ul>
-            <ul style="float: right;" class="navbar-nav ">
-                <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="dangky.html">Đăng kí</a>
-                </li>
-            </ul>
-            <form class="navbar-nav" style="left: 100px;">
-                <div class="form-group">
-                    <input type="text" class="form-control" placeholder="Search">
-                </div>
-                <button type="submit" class="btn btn-default">
-                    <span class="glyphicon glyphicon-search"></span>
-                </button>
-            </form>
-
-        </nav>
-    </header>
+    <?php
+    include('./partials/header.php');
+    ?>
 
     <main>
 
@@ -135,7 +66,7 @@
                     <p>Giới tính: Nam</p>
                     <p>Địa chỉ: 01 An Dương Vương</p>
                     <p>Sdt: 012345678</p>
-                    <p><a class="btn btn-secondary" href="#">Xem chi tiết &raquo;</a></p>
+                    <p><a class="btn btn-secondary" href="chitietnguoidung.php">Xem chi tiết &raquo;</a></p>
                 </div>
                 <!-- /.col-lg-4 -->
                 <div class="col-lg-4">
@@ -200,18 +131,5 @@
         <!-- /.container -->
 
 
-        <!-- FOOTER -->
-        <footer class="container">
-            <p class="float-end">
-                <a href="#" Tuyển nhân sự</a></p>
-            <p>&copy; 2022-2023 Quyền riêng tư. &middot; <a href="#">Điều khoảng</a> &middot; <a href="#">Bảo mật</a></p>
-        </footer>
-    </main>
-
-
-    <script src="../assets/dist/js/bootstrap.bundle.min.js"></script>
-
-
-</body>
-
-</html>
+<?php
+    include './partials/footer.php';
---
+++ partials/header.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="vi">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Trang tuyển dụng</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/carousel/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+        .avt{
+            border-radius:50% ;
+        }
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+            padding: 10px;
+        }
+        
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+        
+        .bg-container {
+            margin-top: 40px;
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <nav style="float: right;" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark navbar-form navbar-left">
+
+            <ul style="float: right;" class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html">Trang chủ</a>
+                </li>
+            </ul>
+            <ul style="float: right;" class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="dangnhap.html">Đăng nhập</a>
+                </li>
+            </ul>
+            <ul style="float: right;" class="navbar-nav ">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="dangky.html">Đăng kí</a>
+                </li>
+            </ul>
+            <form class="navbar-nav" style="left: 100px;">
+                <div class="form-group">
+                    <input type="text" class="form-control" placeholder="Search">
+                </div>
+                <button type="submit" class="btn btn-default">
+                    <span class="glyphicon glyphicon-search"></span>
+                </button>
+            </form>
+
+        </nav>
+    </header>
